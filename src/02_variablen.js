a30eaa4d8a2577f86a7bab2cd876315fb177f2be
MichaelD <atlasbook@web.de>
2025-11-06 11:04:45 +0000

    Variablen vs. Konstanten in JS

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -27,27 +27,37 @@
     JS ist ein untypisierte Sprache!
 */
 
-let test;
+// let test;
 
-/* undefined */
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------")
-/* defined string*/
-test = "hi"; // string
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------")
-/* defined number */
-test = 2; // number
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------")
-/* defined boolean */
-test = true; // boolean
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
-console.log("Inhalt: " + test); // Ausgabe Inhalt
-console.log("----------------")
+// /* undefined */
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
+// /* defined string*/
+// test = "hi"; // string
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
+// /* defined number */
+// test = 2; // number
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
+// /* defined boolean */
+// test = true; // boolean
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
 
+/* Variablen vs. Konstanten in JS */
 
+// Variable
+let test; // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe 
 
+// Konstante
+const test2  = "Hi!"; // Deklaration + Wertzuweisung gemeinsam !
+// test2 = "Hello"; // Laufzeit-Fehler !!| Taucht erst auf, sobald das Programm läuft
+console.log("Inhalt: " + test2); // Ausgabe
