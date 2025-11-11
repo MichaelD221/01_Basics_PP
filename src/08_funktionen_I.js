1e5d12fbd67bc90f843a4821e2ab780c17b7957a
MichaelD <atlasbook@web.de>
2025-11-11 21:05:08 +0000

    09 / challenge vollendet

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -13,86 +13,160 @@ function test()
     console.log("Hallo, Kevin!");
 }
 
-/***** Funktionen 02a *****/
-// 2a. Parametrisierung + Datenübergabe von INNEN
+// /***** Funktionen 02a *****/
+// // 2a. Parametrisierung + Datenübergabe von INNEN
 
-// call
-// ausgabeNamen();
+// // call
+// // ausgabeNamen();
 
-// Funktion
-function ausgabeNamen() {
-    // interne Variable | What happens in VEGAS, stays in VEGAS ...
+// // Funktion
+// function ausgabeNamen() {
+//     // interne Variable | What happens in VEGAS, stays in VEGAS ...
+//     let firstName = "Karsten";
+//     console.log("Hallo, " + firstName + "!");
+// }
+
+// // console.log(firstName); // SCOPE!! Variable von außen nicht sichtbar, weil eingekapselt in "VEGAS"{}
+
+// /***** Funktionen 02b *****/
+// // 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+// //  ausgabeNamen2("Kevin"); // Argument --> Daten für Parameter
+// //  ausgabeNamen2("Klaus");
+// //  ausgabeNamen2("Ibrahim");
+
+// function ausgabeNamen2(firstName) { // Parameter
+//     console.log("Hallo, " + firstName + "!");
+// }
+
+// /***** Funktionen 02c *****/
+// // 2c. Mehrere Parameter / Argumente
+
+// // ausgabeNamen3("Max", "Mütze");
+// // ausgabeNamen3("Maxine", "Mützerich");
+
+// // Argumente werden von prompt() geliefert --> Input
+// // const prompt = require('prompt-sync')({sigint: true});
+// // ausgabeNamen3(prompt("Vorname: "), prompt("Name: "));
+
+
+// function ausgabeNamen3(firstName, familyName) { // Parameter
+//     console.log("Hallo, " + firstName + " " + familyName + "!");
+// }
+
+
+// /***** Funktionen 03a *****/  // refactoring = Maschine umbauen, übersichtliche usw., nach außen bleibt es aber gleich, der Kunde merkt nichts
+// // 03a. Vorbereitung -Trennen der Verantwortlichkeiten
+// // Postulat: one function = one job (uncle Bob)
+// // SRP single responsibility principle // Eduscho, alles bleibt wie es ist, aber Funktionalitäten und Verantwortlichkeiten trennen
+
+// // ausgabeNamenSRP("Max","Mütze");
+
+// function ausgabeNamenSRP(firstName, familyName) { // Parameter
+
+//     //1. Verantwortlichkeit/ 1st responsibility: string composing 
+//     const GAP = " ";
+//     let outputStr = "Hallo, " + firstName + GAP + familyName + "!";
+
+//     //2nd responsibility: string output
+//     console.log(outputStr);
+
+// }
+
+// /***** Funktionen 03b *****/ 
+
+// // 1st responsibility: string composing 
+// output(getString("Max","Mütze")); // baut den String
+// output(getString("Maxine","Mützerich"));
+// const prompt = require('prompt-sync')({sigint: true});
+// output(getString(prompt("Vorname?: "),prompt("Name?: ")));
+
+
+// function getString(firstName,familyName) {
+//     const GAP = " ";
+//     let outputStr = "Hallo, " + firstName + GAP + familyName + "!";
+//     return outputStr; // Funktion - OUTPUT sendet Daten zum call
+// }
+
+// // 2nd responsibility: string output
+// // output("hi");
+// // output(2);
+// // output(true);
+// function output(outputData) { // Wrapper
+//     console.log(outputData); // wird das kommentiert, wird nachfolgender Text deaktiviert
+// }
+
+
+test();
+
+function test()
+{
+    console.log("Hallo, Kevin!");
+}
+///////////////////////////////////////////
+console.log("-----------------------------");
+///////////////////////////////////////////
+ausgabeNamen();
+
+function ausgabeNamen() 
+{
     let firstName = "Karsten";
     console.log("Hallo, " + firstName + "!");
 }
+///////////////////////////////////////////
+console.log("-----------------------------");
+///////////////////////////////////////////
+ausgabeNamen2("Kevin");
+ausgabeNamen2("Klaus");
+ausgabeNamen2("Ibrahim");
 
-// console.log(firstName); // SCOPE!! Variable von außen nicht sichtbar, weil eingekapselt in "VEGAS"{}
-
-/***** Funktionen 02b *****/
-// 2b. Parametrisierung + Datenübergabe von AUSSEN
-
-//  ausgabeNamen2("Kevin"); // Argument --> Daten für Parameter
-//  ausgabeNamen2("Klaus");
-//  ausgabeNamen2("Ibrahim");
-
-function ausgabeNamen2(firstName) { // Parameter
+function ausgabeNamen2(firstName) 
+{
     console.log("Hallo, " + firstName + "!");
 }
+///////////////////////////////////////////
+console.log("-----------------------------");
+///////////////////////////////////////////
+ausgabeNamen3("Max", "Mütze");
+ausgabeNamen3("Maxine", "Mützerich");
+ausgabeNamen3(prompt("Vorname: "), prompt("Name: "));
 
-/***** Funktionen 02c *****/
-// 2c. Mehrere Parameter / Argumente
-
-// ausgabeNamen3("Max", "Mütze");
-// ausgabeNamen3("Maxine", "Mützerich");
-
-// Argumente werden von prompt() geliefert --> Input
-// const prompt = require('prompt-sync')({sigint: true});
-// ausgabeNamen3(prompt("Vorname: "), prompt("Name: "));
-
-
-function ausgabeNamen3(firstName, familyName) { // Parameter
+function ausgabeNamen3(firstName, familyName) 
+{
     console.log("Hallo, " + firstName + " " + familyName + "!");
 }
+///////////////////////////////////////////
+console.log("-----------------------------");
+///////////////////////////////////////////
+ausgabeNamenSRP("Max","Mütze");
 
-
-/***** Funktionen 03a *****/  // refactoring = Maschine umbauen, übersichtliche usw., nach außen bleibt es aber gleich, der Kunde merkt nichts
-// 03a. Vorbereitung -Trennen der Verantwortlichkeiten
-// Postulat: one function = one job (uncle Bob)
-// SRP single responsibility principle // Eduscho, alles bleibt wie es ist, aber Funktionalitäten und Verantwortlichkeiten trennen
-
-// ausgabeNamenSRP("Max","Mütze");
-
-function ausgabeNamenSRP(firstName, familyName) { // Parameter
-
-    //1. Verantwortlichkeit/ 1st responsibility: string composing 
+function ausgabeNamenSRP(firstName, familyName) 
+{
     const GAP = " ";
     let outputStr = "Hallo, " + firstName + GAP + familyName + "!";
-
-    //2nd responsibility: string output
     console.log(outputStr);
-
 }
 
-/***** Funktionen 03b *****/ 
 
-// 1st responsibility: string composing 
-output(getString("Max","Mütze")); // baut den String
+
+output(getString("Max","Mütze"));
 output(getString("Maxine","Mützerich"));
-const prompt = require('prompt-sync')({sigint: true});
 output(getString(prompt("Vorname?: "),prompt("Name?: ")));
 
-
-function getString(firstName,familyName) {
+function getString(firstName,familyName) 
+{
     const GAP = " ";
     let outputStr = "Hallo, " + firstName + GAP + familyName + "!";
-    return outputStr; // Funktion - OUTPUT sendet Daten zum call
+    return outputStr;
 }
 
-// 2nd responsibility: string output
-// output("hi");
-// output(2);
-// output(true);
-function output(outputData) { // Wrapper
-    console.log(outputData); // wird das kommentiert, wird nachfolgender Text deaktiviert
+output("hi");
+output(2);
+output(true);
+
+function output(outputData) 
+{
+    console.log(outputData);
 }
+
 
--- src/09_codechallenge_III .js
+++ src/09_codechallenge_III .js
@@ -10,7 +10,118 @@
 
 const prompt = require('prompt-sync')({sigint: true});
 
-function Burger(Hamburger, Cheeseburger, Chilliburger);
+let food, drink, decision;
+
+burger()
+
+function burger()
 {
+    console.log("Herzlich Willkommen bei McDonalds - Ihre Bestellung bitte!");
+    console.log("Möchten Sie einen Hamburger (1), einen Cheeseburger (2) oder einen Chilliburger (3)?");
+    decision = prompt("Geben Sie die dazugehörige Ziffer ein: ");
+    const cond = true;
+    switch (cond) {
+        case (decision == 1):
+            food = "Hamburger"; 
+            break;
+        case (decision == 2):
+            food = "Cheeseburger"; 
+            break;
+        case (decision == 3):
+            food = "Chilliburger"; 
+            break;
+        default:
+            food = " ";
+            break;
+    }
 
 }
+
+getränk()
+
+function getränk()
+{
+    console.log("Möchten Sie ein Getränk?");
+    decision = prompt("j/n?: ");
+    const cond = true;
+
+    if(decision == "j")
+    {
+        decision = prompt("Möchten Sie Cola (1), Fanta (2) oder Sprite (3)?: ");
+        switch (cond) 
+        {
+            case (decision == 1):
+                drink = "Cola";
+                break;
+            case (decision == 2):
+                drink = "Fanta";
+                break;
+            case (decision == 3):
+                drink = "Sprite";
+                break;
+            default:
+                drink = " ";
+                break;
+        }
+    }
+    else
+    {
+        drink = " ";
+    }
+}
+
+console.log("Ihre Bestellung: " + food + " / " + drink);
+console.log("Vielen Dank und auf Wiedersehen");
+
+
+
+// const prompt = require('prompt-sync')({sigint: true});
+
+// let food = "";
+// let drink = "";
+// let wahl
+
+// console.log("Guten Tag bei MD, Ihre Bestellung bitte!");
+// console.log("Hamburger (1), Cheeseburger (2) oder Chilliburger (3)?");
+
+// wahl = prompt("Bitte Nummer eingeben:");
+
+// switch (wahl) {
+//   case "1":
+//     food = "Hamburger";
+//     break;
+//   case "2":
+//     food = "Cheeseburger";
+//     break;
+//   case "3":
+//     food = "Chilliburger";
+//     break;
+//   default:
+//     food = " ";
+// }
+
+// wahl = prompt("Möchten Sie ein Getränk dazu bestellen? (j/n)");
+
+// if (wahl === "j") {
+//   console.log("Cola (1), Fanta (2), Sprite (3)?");
+//   wahl = prompt("Bitte Nummer eingeben:");
+
+//   switch (wahl) {
+//     case "1":
+//       drink = "Cola";
+//       break;
+//     case "2":
+//       drink = "Fanta";
+//       break;
+//     case "3":
+//       drink = "Sprite";
+//       break;
+//     default:
+//       drink = " ";
+//   }
+//   console.log("Hier ist Ihre Bestellung: " + food + " / " + drink);
+// } else {
+//   console.log("Hier ist Ihre Bestellung: " + food);
+// }
+
+// console.log("Vielen Dank, und Auf Wiedersehen!");
