25de4f45374229093ffddd9004c12e8812ef3669
MichaelD <atlasbook@web.de>
2025-11-13 09:59:44 +0000

    11 / Theorie: Array

---
+++ src/11_arrays_I.js
@@ -0,0 +1,59 @@
+
+/* Die Satzbau-Maschine | Arrays */
+
+/**
+ *   THEORIE
+ * 
+ */
+
+/*** 02. Theorie: Array */
+
+let arr;
+// arr = new Array(); // Konstruktor
+arr = [];
+arr = [2,11,7,8,10,20];
+
+output(arr);
+output(arr.length); // Anzahl der Elemente
+output(arr[0]); // Index 0 (1. Pos. im Array)
+output(arr[arr.length-1]); // letzte Position / allgemein
+
+
+
+
+
+
+/**
+ *   PRAXIS
+ * 
+ */
+
+
+/*** 01. Funktionalität mit Einzelparametern */
+// --> "Ich bin Max Mütze."
+// output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+// function getSentence(word1,word2,word3,word4, word5, word6) {
+//     const GAP = " ";
+//     const PUNCT = ".";
+//     let str =   word1 + GAP +
+//                 word2 + GAP +
+//                 word3 + GAP +
+//                 word4 + GAP +
+//                 word5 + GAP +
+//                 word6 +
+//                 PUNCT;
+
+//     return str;
+// }
+
+
+
+
+// Modul: Ausgabe in Konsole : Test
+// output("hi"); 
+function output(outputStr) {
+    console.log(outputStr);
+}
+
+
