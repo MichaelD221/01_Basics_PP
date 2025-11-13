3f2afc296645ce03cc0032ff95d866019a2e45a4
MichaelD <atlasbook@web.de>
2025-11-13 20:41:20 +0000

    12 / challende vollendet

--- src/12_codechallenge_IV.js
+++ src/12_codechallenge_IV.js
@@ -17,9 +17,33 @@ output(getSentence(["Ich","bin","Max"],"S"));
 output(getSentence(["Bist","du","Max"],"Q"));
 output(getSentence(["Ich","bin"],"E"));
 output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+output(getSentence(["Drei","Chinesen","mit","'nem","Kontrabass"]));
 
 function getSentence(arr,op) {
    // ???
+
+    const GAP = " ";
+    const SEN = ".";
+    const QUE = "?";
+    const EXP = "!";
+
+    let text = arr.join(GAP);
+
+    switch (op) 
+    {
+        case "S": 
+            text += SEN;
+            break;
+        case "Q":
+            text += QUE;
+            break;
+        case "E":
+            text += EXP;
+            break;
+        default:
+            text += SEN;
+    }
+    return text;
 }
 
 
