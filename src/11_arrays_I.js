ef486f2707dafe63cd904561025ad715a9c92753
MichaelD <atlasbook@web.de>
2025-11-13 11:02:46 +0000

    11 / For-Schleife

--- src/11_arrays_I.js
+++ src/11_arrays_I.js
@@ -8,16 +8,41 @@
 
 /*** 02. Theorie: Array */
 
-let arr;
-// arr = new Array(); // Konstruktor
-arr = [];
-arr = [2,11,7,8,10,20];
+// let arr;
+// // arr = new Array(); // Konstruktor
+// arr = [];
+// arr = [2,11,7,8,10,20];
 
-output(arr);
-output(arr.length); // Anzahl der Elemente
-output(arr[0]); // Index 0 (1. Pos. im Array)
-output(arr[arr.length-1]); // letzte Position / allgemein
+// output(arr);
+// output(arr.length); // Anzahl der Elemente
+// output(arr[0]); // Index 0 (1. Pos. im Array)
+// output(arr[arr.length-1]); // letzte Position / allgemein
 
+/* 02a. Theorie: Schleifen (for-schleife) */
+/* For -Schleifen als allg. Wiederholungs-Struktur */
+ 
+// Inkrement (untere Grenze --> obere Grenze)
+// for (let i = 0; i < 100 ; i++) {
+//     output("index : " + i); // i --> Schleifen-Index
+// }
+
+// Dekrement (obere Grenze --> untere Grenze)
+// for (let i = 10; i > 0; i--) {  // i --> Schleifen-Index
+//     output("index :" + i); 
+// }
+
+// Inkrement (var. Schrittweite)
+// for (let i = 10; i <= 100; i += 10) {
+//    output("index :" + i);  
+// }
+
+/* 02b. For-Schleife für Array-Index (Iteration)*/
+let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+for (let i = 0; i < arr.length; i++) 
+{
+//    output(i); // interner Index 
+   output(arr[i]);  // Mapping  intern --> extern
+}
 
 
 
@@ -27,6 +52,8 @@ output(arr[arr.length-1]); // letzte Position / allgemein
  *   PRAXIS
  * 
  */
+
+
 
 
 /*** 01. Funktionalität mit Einzelparametern */
@@ -46,6 +73,26 @@ output(arr[arr.length-1]); // letzte Position / allgemein
 
 //     return str;
 // }
+
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] +
+                PUNCT;
+
+    return str;
+}
 
 
 
